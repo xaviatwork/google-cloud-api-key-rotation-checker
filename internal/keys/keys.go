2decfc0dc666326cc1fe797adc50a18fa61da85f
Xavi Aznar <xavier.aznar@seat.es>
2026-04-26 10:35:19 +0000

    refactor: reorganizing the code

---
+++ internal/keys/keys.go
@@ -0,0 +1,55 @@
+package keys
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	apikeys "cloud.google.com/go/apikeys/apiv2"
+	"cloud.google.com/go/apikeys/apiv2/apikeyspb"
+)
+
+type Key struct {
+	CreateTime  time.Time `json:"create_time,omitempty"`
+	DisplayName string    `json:"display_name,omitempty"`
+	Name        string    `json:"name,omitempty"`
+	ProjectId   string    `json:"project_id,omitempty"`
+}
+
+func (k Key) String() string {
+	return fmt.Sprintf("%s on project %s (created: %s)", k.DisplayName, k.ProjectId, k.CreateTime.Format(time.RFC1123))
+}
+
+func List(projectid string) []*Key {
+	ctx := context.Background()
+	c, err := apikeys.NewClient(ctx)
+	if err != nil {
+		fmt.Printf("new client error: %s\n", err.Error())
+		os.Exit(1)
+	}
+	defer c.Close()
+
+	req := &apikeyspb.ListKeysRequest{
+		// See https://pkg.go.dev/cloud.google.com/go/apikeys/apiv2/apikeyspb#ListKeysRequest.
+		Parent: fmt.Sprintf("projects/%s/locations/global", projectid),
+	}
+
+	var keys []*Key
+	for k, err := range c.ListKeys(ctx, req).All() {
+		if err != nil {
+			// TODO: Handle error and break/return/continue. Iteration will stop after any error.
+			fmt.Printf("list keys error: %s\n", err.Error())
+			os.Exit(1)
+		}
+		key := &Key{
+			Name:        k.Name,
+			DisplayName: k.DisplayName,
+			CreateTime:  k.CreateTime.AsTime(),
+			ProjectId:   projectid,
+		}
+		keys = append(keys, key)
+	}
+
+	return keys
+}
--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"os"
 
-	apikeys "cloud.google.com/go/apikeys/apiv2"
-	apikeyspb "cloud.google.com/go/apikeys/apiv2/apikeyspb"
+	"github.com/xaviatwork/api-key-checker/internal/keys"
 )
 
 func main() {
@@ -23,24 +21,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	ctx := context.Background()
-	c, err := apikeys.NewClient(ctx)
-	if err != nil {
-		fmt.Printf("new client error: %s\n", err.Error())
-		os.Exit(1)
-	}
-	defer c.Close()
+	projectIds := []string{*projectId}
 
-	req := &apikeyspb.ListKeysRequest{
-		// See https://pkg.go.dev/cloud.google.com/go/apikeys/apiv2/apikeyspb#ListKeysRequest.
-		Parent: fmt.Sprintf("projects/%s/locations/global", *projectId),
+	// get API key list
+	var keylist []*keys.Key
+	for _, p := range projectIds {
+		kk := keys.List(p)
+		keylist = append(keylist, kk...)
 	}
-	for k, err := range c.ListKeys(ctx, req).All() {
-		if err != nil {
-			// TODO: Handle error and break/return/continue. Iteration will stop after any error.
-			fmt.Printf("list keys error: %s\n", err.Error())
-			os.Exit(1)
-		}
-		fmt.Println(k.DisplayName)
+
+	// display API keys
+	for _, k := range keylist {
+		fmt.Println(k.String())
 	}
 }
